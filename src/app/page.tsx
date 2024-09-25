7fc087c2b13a7b1db0737012a82668857d601480
Aakash Thakur <aakash.thakur@nexthink.com>
2024-09-25 11:24:56 +0530

    basic convex schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
+  const createTask = useMutation(api.tasks.createTask);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+
+      <button onClick={() => createTask({ text: "New Task" })}>
+        Create task
+      </button>
 
       <SignedOut>
         <SignInButton />
